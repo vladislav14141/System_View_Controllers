c40924c53a2b210cf9e481f50691417d458f2a7f
Vladislav <vladislav14141@yandex.ru>
2019-04-20 17:55:27 +1000

    Initial Commit

---
+++ System_View_Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  System_View_Controllers
+//
+//  Created by Миронов Влад on 20/04/2019.
+//  Copyright © 2019 Миронов Влад. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
